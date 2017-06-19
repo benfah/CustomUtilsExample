b3a85df26b1c42aed17ec19b9d5e963d3f73a83b
benfah <vayegame@gmail.com>
2017-06-19 21:35:41 +0200

    Initial commit

---
+++ com/example/MyPlugin.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.benfah.cu.api.CustomBlock;
+import me.benfah.cu.api.CustomRegistry;
+
+public class MyPlugin extends JavaPlugin
+{
+	public static MyPlugin instance;
+	public static final RFMachine rfMachine = new RFMachine();
+	public static final RFMenu rfMenu = new RFMenu();
+	@Override
+	public void onEnable() {
+		this.instance = this;
+		CustomRegistry.registerBlock(rfMachine, this);
+		CustomRegistry.registerGUI(rfMenu, this);
+		
+	}
+	
+	@Override
+	public void onLoad() {
+		super.onLoad();
+	}
+	
+	
+}
